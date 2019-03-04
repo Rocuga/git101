7ec9d035fdfecd386553a019997a2523cd2efdc4
Riccardo Potenza <rickelite94@hotmail.it>
2019-03-04 12:44:25 +0100

    massimaze size description

--- humansize.py
+++ humansize.py
@@ -1,6 +1,12 @@
 SUFFIXES = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
 def approximate_size(size):
+	"""Convert a file size to human_readable form.
+	keyword arguments:
+	size -- file size in bytes
+
+	Returns: string
+	"""
 	multiple = 1024
 	for suffix in SUFFIXES:
 		size /= multiple
